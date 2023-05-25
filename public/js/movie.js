3b1f5bcb1bfd41103109ef5ebe48e9b4dc65aa4e
xodmf1215 <xodmf1215@users.noreply.github.com>
2023-05-26 00:59:53 +0900

    complete

--- public/js/movie.js
+++ public/js/movie.js
@@ -2,7 +2,7 @@ const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?api_key=01057de4f46063e362a4e64cc526e8f9&query=";
 
-const REVIEWAPI = "http://localhost:3000/api/v1/movie/"
+const REVIEWAPI = "http://localhost:3000/api/v1/reviews/"
 
 // make a url object which help us use url
 const url = new URL(location.href);
@@ -14,9 +14,11 @@ const title = document.getElementById("movie-title");
 
 title.innerText = movieTitle;
 
+
+
 returnReviews(REVIEWAPI);
 function returnReviews(url) {
-  fetch(url + movieId).then(res => res.json())
+  fetch(url + "movie/" + movieId).then(res => res.json())
   .then( function(data) {
     data.forEach(review => {
       const div_card = document.createElement('div');
@@ -27,12 +29,93 @@ function returnReviews(url) {
               <p><strong>Review: </strong>${ review.review }</p>
               <p><strong>User: </strong>${ review.user }</p>
               <p><a href="#" onclick="editReview('${ review._id }', '${review.review }',
-'${ review.user }')">write</a> <a href="#" onclick="deleteReview('${ review._id }')">delete</a>/p>
+'${ review.user }')">✏️</a> <a href="#" onclick="deleteReview('${ review._id }')">🗑️</a></p>
             </div>
           </div>
-        </div>
-      `;
+        </div>`;
       main.appendChild(div_card);
     })
-  } )
+  } );
+  const div_addReview = document.createElement('div');
+  div_addReview.innerHTML = `
+    <form id="form_review">
+      <label for="review_user"> user:</label>
+      <input id="review_user" type="text"><br>
+      <label for="review_content">content:</label>
+      <input id="review_content" type="text"><br>
+      <button type="submit"  onclick="addReview(event)">add review</button>
+    </form>
+  `;
+  main.appendChild(div_addReview);
+}
+
+function editReview(id, review, user) {
+  const element = document.getElementById(id);
+  const reviewInputId = "review"+id;
+  const userInputId = "user" +id;
+
+  element.innerHTML = `
+    <p>
+      <strong>Review: </strong>
+      <input type="text" id="${ reviewInputId }" value="${ review }">
+    </p>
+    <p>
+      <strong>User: </strong>
+      <input type="text" id="${ userInputId }" value="${ user }">
+    </p>
+    <p>
+      <a href="#" onclick="saveReview('${ reviewInputId}', '${ userInputId }', '${ id }')">💾</a>
+    </p>`;
+}
+
+function saveReview(reviewInputId, userInputId, id) {
+  const review = document.getElementById(reviewInputId).value;
+  const user = document.getElementById(userInputId).value;
+  fetch(REVIEWAPI + id, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ "user": user, "review": review }),
+  }).then(response => {
+    response.json();
+  }).then(json => {
+    console.log(json);
+    location.reload();
+  })
+}
+
+function deleteReview(id) {
+  fetch(REVIEWAPI + id, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
+  }).then(response => {
+    response.json();
+  }).then(json => {
+    console.log(json);
+    location.reload();
+  })
+}
+
+function addReview(event) {
+  event.preventDefault();
+  const review = document.getElementById('review_content').value;
+  const user = document.getElementById('review_user').value;
+  fetch(REVIEWAPI + 'new/', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ "movieId": parseInt(movieId), "user": user, "review": review }),
+  }).then(response => {
+    response.json();
+  }).then(json => {
+    console.log(json);
+    location.reload();
+  })
 }
